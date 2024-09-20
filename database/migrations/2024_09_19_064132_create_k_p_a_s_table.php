57a32abec106596eef017048a207d01149032d18
cloudmyn <cloudmyn46@gmail.com>
2024-09-20 15:12:21 +0800

    add fitur

---
+++ database/migrations/2024_09_19_064132_create_k_p_a_s_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('k_p_a_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('nip')->unique();
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('k_p_a_s');
+    }
+};
---
+++ database/migrations/2024_09_19_064144_create_p_p_k_s_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('p_p_k_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('nip')->unique();
+            $table->string('position');
+            $table->string('working_unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('p_p_k_s');
+    }
+};
