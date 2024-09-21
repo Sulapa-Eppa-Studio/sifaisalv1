b3422cb309db9a642bb6258bb4f71a93fc0190e1
Alwi Jaya <alwijein@gmail.com>
2024-09-21 16:39:09 +0800

    ✨ feat: Redirect To Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // redirect to the admin panel
+
+    if (Auth::guest()) {
+        return redirect('/login');
+    }
+
     switch (Auth::user()->role) {
         case 'admin':
             return redirect('/admin');
