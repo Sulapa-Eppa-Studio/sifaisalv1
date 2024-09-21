8167278951d6f4ab6dd3ced1b30d934da2e2ece4
Alwi Jaya <alwijein@gmail.com>
2024-09-21 16:37:45 +0800

    ✨ feat: Redirect To Admin Panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,9 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\KpaPanelProvider::class,
     App\Providers\Filament\PenyediaJasaPanelProvider::class,
+    App\Providers\Filament\PpkPanelProvider::class,
+    App\Providers\Filament\SpmPanelProvider::class,
+    App\Providers\Filament\TreasurerPanelProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,35 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // redirect to the admin panel
-    return redirect('/admin');
+    switch (Auth::user()->role) {
+        case 'admin':
+            return redirect('/admin');
+            break;
+        case 'penyedia_jasa':
+            return redirect('/penyediaJasa');
+            break;
+        case 'ppk':
+            return redirect('/ppk');
+            break;
+
+        case 'kpa':
+            return redirect('/kpa');
+            break;
+        case 'spm':
+            return redirect('/spm');
+            break;
+        case 'service_provider':
+            return redirect('/serviceProvider');
+            break;
+        case 'treasurer':
+            return redirect('/treasurer');
+            break;
+        default:
+            return redirect('/login');
+            break;
+    }
 });
