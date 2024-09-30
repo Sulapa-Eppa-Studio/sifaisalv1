bc90f6e6f54415737e44b5ac0711f88f945a0c23
Alwi Jaya <alwijein@gmail.com>
2024-09-30 17:15:27 +0800

    Refactor Filament panel KPA providers and add KPA dashboard module

---
+++ app/Filament/Kpa/Pages/Dashboard.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Kpa\Pages;
+
+use App\Filament\PenyediaJasa\Widgets\AccountWidget;
+use App\Filament\Ppk\Widgets\PaymentStaticsCard;
+use App\Filament\Ppk\Widgets\PaymentVerificationStatusChart;
+use App\Filament\Treasurer\Widgets\SPMRequestStatusChart;
+use Filament\Facades\Filament;
+use Filament\Pages\Page;
+use Filament\Support\Facades\FilamentIcon;
+use Illuminate\Contracts\Support\Htmlable;
+
+class Dashboard extends Page
+{
+    protected static string $routePath = '/';
+
+    protected static ?int $navigationSort = -2;
+
+    /**
+     * @var view-string
+     */
+    protected static string $view = 'filament-panels::pages.dashboard';
+
+
+    public static function getNavigationLabel(): string
+    {
+        return 'Dashboard';
+    }
+
+    public static function getNavigationIcon(): string | Htmlable | null
+    {
+        return static::$navigationIcon
+            ?? FilamentIcon::resolve('panels::pages.dashboard.navigation-item')
+            ?? (Filament::hasTopNavigation() ? 'heroicon-m-home' : 'heroicon-o-home');
+    }
+
+    /**
+     * @return array<Action | ActionGroup>
+     */
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+
+    public static function getRoutePath(): string
+    {
+        return static::$routePath;
+    }
+
+    /**
+     * @return array<class-string<Widget> | WidgetConfiguration>
+     */
+    public function getWidgets(): array
+    {
+        return [
+            AccountWidget::class,
+            PaymentStaticsCard::class,
+            PaymentVerificationStatusChart::class,
+            SPMRequestStatusChart::class,
+        ];
+    }
+
+    /**
+     * @return array<class-string<Widget> | WidgetConfiguration>
+     */
+    public function getVisibleWidgets(): array
+    {
+        return $this->filterVisibleWidgets($this->getWidgets());
+    }
+
+    /**
+     * @return int | string | array<string, int | string | null>
+     */
+    public function getColumns(): int | string | array
+    {
+        return 2;
+    }
+
+    public function getTitle(): string | Htmlable
+    {
+        return 'Dashboard';
+    }
+}
