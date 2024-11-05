4ee5f308e158de3e5818e719aee94f4a6fa4ca2b
cloudmyn <cloudmyn46@gmail.com>
2024-11-05 18:15:58 +0800

    update

--- app/Filament/Spm/Resources/PaymentRequestResource.php
+++ app/Filament/Spm/Resources/PaymentRequestResource.php
@@ -464,10 +464,12 @@ class PaymentRequestResource extends Resource
                     ->action(function (PaymentRequest $record, array $data) {
 
                         $record->update([
-                            'ppspm_verification_status'   =>  'rejected',
-                            'verification_progress'       =>  'rejected',
-                            'ppspm_rejection_reason'      =>  $data['reject_reason'],
-                            'ppspm_id'                    =>  get_auth_user()->spm->id,
+                            'verification_progress'         =>  'rejected',
+                            'ppk_verification_status'       =>  'rejected',
+                            'ppk_rejection_reason'          =>  'Ditolak oleh SPM',
+                            'ppspm_verification_status'     =>  'rejected',
+                            'ppspm_rejection_reason'        =>  $data['reject_reason'],
+                            'ppspm_id'                      =>  get_auth_user()->spm->id,
                         ]);
 
                         Notification::make()
